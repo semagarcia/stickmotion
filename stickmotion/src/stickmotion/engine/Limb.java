41f3b5b2c35d37b7e28788e9bcc59d589c1b0c21
ferkiwi <ferkiwi@2c9bdd84-2b83-11df-8484-2f92f965d510>
2010-03-15 16:58:31 +0000

    Arreglado el warning que tenía y editado la configuración de  Eclipse para tener un estilo de codificación común entre todos (se ajusta sólo al guardar)

--- stickmotion/src/stickmotion/engine/Limb.java
+++ stickmotion/src/stickmotion/engine/Limb.java
@@ -4,56 +4,66 @@
 package stickmotion.engine;
 
 /**
- * Clase de la que heredarán el resto de extremidades 
- *
+ * Clase genérica para la declaración de las secciones del stickman de la que
+ * heredarán el resto de extremidades
  */
 public class Limb {
 
-	private double angle;
-	private double size;
-	
-	/**
-	 * @throws WrongSizeException 
-	 * 
-	 */
-	public Limb(double angle, double size) throws WrongSizeException {
-		setAngle(angle);
-		setSize(size);
-	}
+  private double angle;
+  private double size;
 
+  /**
+   * Constructor de la clase Limb
+   * 
+   * @param angle
+   * @param size
+   * @throws WrongSizeException
+   */
+  public Limb(double angle, double size) throws WrongSizeException {
+    setAngle(angle);
+    setSize(size);
+  }
 
-	/**
-	 * @param angle the angle to set
-	 */
-	public void setAngle(double angle) {
-		this.angle = angle;
-	}
+  /**
+   * Método de asignación del tamaño
+   * 
+   * @param size
+   *          El valor en tanto por uno del tamaño a asignar
+   * @throws WrongSizeException
+   */
+  public void setSize(double size) throws WrongSizeException {
+    // El tanto por uno de tamaño debe ser entre 0 y 1
+    if (size > 1 || 0 < size)
+      throw new WrongSizeException();
+    this.size = size;
+  }
 
+  /**
+   * Método de obtención del tamaño
+   * 
+   * @return devuelve el valor de tamaño
+   */
+  public double getSize() {
+    return size;
+  }
 
-	/**
-	 * @return the angle
-	 */
-	public double getAngle() {
-		return angle;
-	}
+  /**
+   * Método de asignación del ángulo
+   * 
+   * @param angle
+   *          El valor del ángulo a asignar
+   */
+  public void setAngle(double angle) {
+    this.angle = angle;
+  }
 
-	/**
-	 * @param size the size to set
-	 * @throws WrongSizeException 
-	 */
-	public void setSize(double size) throws WrongSizeException {
-		if(size > 1 || 0 < size) {
-			throw new WrongSizeException();
-		}
-		this.size = size;
-	}
+  /**
+   * Método de obtención del ángulo
+   * 
+   * @return devuelve el valor de ángulo
+   */
+  public double getAngle() {
+    return angle;
+  }
 
-
-	/**
-	 * @return the size
-	 */
-	public double getSize() {
-		return size;
-	}
-	
 }
