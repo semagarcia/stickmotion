68a14a6ae9972d13eb2c61d632d26c3299018ba3
ferkiwi <ferkiwi@2c9bdd84-2b83-11df-8484-2f92f965d510>
2010-03-15 16:58:31 +0000

    Arreglado el warning que tenía y editado la configuración de  Eclipse para tener un estilo de codificación común entre todos (se ajusta sólo al guardar)

--- trunk/stickmotion/src/Main.java
+++ trunk/stickmotion/src/Main.java
@@ -1,11 +1,22 @@
-
+/**
+ * Clase principal de la aplicación
+ * 
+ * Desde aquí se realiza la gestión de los argumentos de linea de comandos que
+ * recibirá el programa
+ */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
+  /**
+   * Método principal
+   * 
+   * @param args
+   *          argumentos de linea de comandos
+   */
+  public static void main(String[] args) {
+    if (args.length < 2)
+      System.out.println("Sintaxis: stickmotion [opciones] [accion]");
+    else {
 
+    }
+  }
 }
--- trunk/stickmotion/src/stickmotion/engine/WrongSizeException.java
+++ trunk/stickmotion/src/stickmotion/engine/WrongSizeException.java
@@ -8,4 +8,9 @@ package stickmotion.engine;
  */
 public class WrongSizeException extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1754128277899329901L;
+
 }
