8e3fe88adb334f9f8eb147c053ab59b1dffd0b30
ferkiwi <ferkiwi@2c9bdd84-2b83-11df-8484-2f92f965d510>
2010-03-17 17:00:52 +0000

    Añadido fondo

--- stickmotion/src/stickmotion/display/Universe3D.java
+++ stickmotion/src/stickmotion/display/Universe3D.java
@@ -3,21 +3,32 @@
  */
 package stickmotion.display;
 
+import java.awt.Font;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Background;
+import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
+import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.Font3D;
+import javax.media.j3d.FontExtrusion;
 import javax.media.j3d.GeometryArray;
 import javax.media.j3d.LineArray;
+import javax.media.j3d.Material;
 import javax.media.j3d.Shape3D;
+import javax.media.j3d.Text3D;
 import javax.media.j3d.TransformGroup;
 import javax.vecmath.Color3f;
+import javax.vecmath.Point3d;
 import javax.vecmath.Point3f;
 
 import com.sun.j3d.loaders.Scene;
 import com.sun.j3d.loaders.objectfile.ObjectFile;
 import com.sun.j3d.utils.behaviors.keyboard.KeyNavigatorBehavior;
+import com.sun.j3d.utils.image.TextureLoader;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 
 /**
@@ -46,16 +57,43 @@ public class Universe3D extends SimpleUniverse {
     BranchGroup objRoot = new BranchGroup();
 
     objRoot.addChild(createLand());
+    objRoot.addChild(createBackground());
 
     TransformGroup vpTrans = getViewingPlatform().getViewPlatformTransform();
     KeyNavigatorBehavior keyNavBeh = new Keybindings(vpTrans);
     objRoot.addChild(keyNavBeh);
+
+    objRoot.addChild(createText("<-- Stickmotion 3D -->"));
 
     // Realiza optimizaciones en este grafo de escena que compone el entorno.
     objRoot.compile();
 
     // Añade la escena creada
     addBranchGraph(objRoot);
+  }
+
+  /**
+   * 
+   * Método para crear el fondo de pantalla
+   * 
+   * @return
+   * 
+   */
+  public Background createBackground() {
+
+    // Esfera circundante sobre la que se dibujará el fondo
+    BoundingSphere boundingSphere = new BoundingSphere(new Point3d(0.0, 0.0,
+        0.0), 100.0);
+    // Carga la textura para el fondo
+    TextureLoader backgroundTexture = new TextureLoader("images/bg.jpg",
+        getCanvas());
+
+    // Background background = new Background(backgroundTexture.getImage());
+    Background background = new Background();
+    background.setColor(new Color3f(0.0f, 0.10f, 0.75f));
+    background.setApplicationBounds(boundingSphere);
+
+    return background;
   }
 
   /**
@@ -83,6 +121,32 @@ public class Universe3D extends SimpleUniverse {
       landGeom.setColor(i, c);
 
     return new Shape3D(landGeom);
+  }
+
+  /**
+   * Método para añadir texto en 3D al entorno
+   * 
+   * @param str
+   * @return
+   */
+  public Shape3D createText(String str) {
+
+    Appearance textAppear = new Appearance();
+    ColoringAttributes textColor = new ColoringAttributes();
+    textColor.setColor(1.0f, 0.0f, 0.0f);
+    textAppear.setColoringAttributes(textColor);
+    textAppear.setMaterial(new Material());
+
+    // Create a simple shape leaf node, add it to the scene graph.
+    Font3D font3D = new Font3D(new Font("Helvetica", Font.PLAIN, 1),
+        new FontExtrusion());
+    Text3D textGeom = new Text3D(font3D, new String(str));
+    textGeom.setAlignment(Text3D.ALIGN_CENTER);
+    Shape3D textShape = new Shape3D();
+    textShape.setGeometry(textGeom);
+    textShape.setAppearance(textAppear);
+
+    return textShape;
   }
 
   /**
