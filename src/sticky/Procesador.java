9cbdbbf1bbae3a30ebd13e9c36a2cff8ab508461
javier.pb86 <javier.pb86@gmail.com>
2010-04-28 11:44:28 +0000

    Javi prueba

--- src/sticky/Procesador.java
+++ src/sticky/Procesador.java
@@ -7,10 +7,7 @@ public class Procesador {
 
   public static void main(String args[]) {
     try {
-      // cambio david
-      // javi
       // Charges a file
-      // cambio
       String file = "codigo.stk";
       FileInputStream fis = new FileInputStream(file);
 
