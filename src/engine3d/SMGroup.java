efdb5fd08fc0aa26c44aec934d10f03f52b38b66
ferkiwi <ferkiwi@2c9bdd84-2b83-11df-8484-2f92f965d510>
2010-04-29 14:56:24 +0000

    Creado el primer stickman!!  (se llama steve)
    Notas: 
    
    * Necesito la función SMGroup que corrigió Fran, para poder rotar los brazos y piernas sobre si mismos y así hacer que uno sea espejo del otro en cuanto a orientación.
    
    * Hay que mirar el escalado para poder reducir el tamaño del stickman, ahora mismo no cabe en la pantalla de lo grande que es.

--- src/engine3d/SMGroup.java
+++ src/engine3d/SMGroup.java
@@ -6,18 +6,18 @@ import javax.media.j3d.TransformGroup;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
 
-// This class is a Group that contains a group of objects and its
-// transformations
+/**
+ * This class is a Group that contains a group of objects and its
+ * transformations
+ */
 public class SMGroup extends Group {
 
-  private final Group _group;
   private final TransformGroup _rotation;
   private final TransformGroup _translation;
   private final TransformGroup _scale;
 
   // constructor that creates a group with its transformations
-  public SMGroup(Group group) {
-    _group = group;
+  public SMGroup() {
     _rotation = new TransformGroup();
     _translation = new TransformGroup();
     _scale = new TransformGroup();
@@ -25,18 +25,39 @@ public class SMGroup extends Group {
     addChild(_translation);
     _translation.addChild(_rotation);
     _rotation.addChild(_scale);
-    _scale.addChild(_group);
 
   }
 
-  // set the translation parameters
+  /**
+   * Method for adding a child to the tree so that transformations can be
+   * performed over it.
+   * 
+   * @param group
+   */
+  public void addSMChild(Group group) {
+    _scale.addChild(group);
+  }
+
+  /**
+   * Method for setting the translation parameters
+   * 
+   * @param x
+   * @param y
+   * @param z
+   */
   public void translation(float x, float y, float z) {
     Transform3D translate = new Transform3D();
     translate.setTranslation(new Vector3f(x, y, z));
     _translation.setTransform(translate);
   }
 
-  // set the rotation parameters
+  /**
+   * Method for setting the rotation parameters
+   * 
+   * @param x
+   * @param y
+   * @param z
+   */
   public void rotation(float x, float y, float z) {
     Transform3D rotX = new Transform3D();
     Transform3D rotY = new Transform3D();
@@ -51,7 +72,13 @@ public class SMGroup extends Group {
     _rotation.setTransform(rotX);
   }
 
-  // set the scale parameters
+  /**
+   * Method for setting the scale parameters
+   * 
+   * @param x
+   * @param y
+   * @param z
+   */
   public void scale(float x, float y, float z) {
     Transform3D scale = new Transform3D();
     scale.setScale(new Vector3d(x, y, z));
