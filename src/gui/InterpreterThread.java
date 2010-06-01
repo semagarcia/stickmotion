f48e79e7a24eaeed73b6d05d2e0991759ddc8445
ferkiwi <ferkiwi@2c9bdd84-2b83-11df-8484-2f92f965d510>
2010-06-01 10:18:54 +0000

    Corregido el problema del thread (no se estaba ejecutando como hilo) y el de cargar la scene desde el hilo

--- src/gui/InterpreterThread.java
+++ src/gui/InterpreterThread.java
@@ -1,30 +1,41 @@
 package gui;
 
 /**
- *
+ * Thread for interpreting the code and loading the animation in the StickMotion
+ * interface
  */
 public class InterpreterThread extends Thread {
 
-  private String _code;
-  private int _debugMode;
+  private final String _code;
+  private final int _debugMode;
   private String _results;
-  private StickMotion _sm;
+  private final StickMotion _sm;
 
-  InterpreterThread() {
+  /**
+   * Constructor for the class InterpreterThread
+   * 
+   * @param sm
+   *          Stickmotion Window interface containing the scene and output box
+   * @param text
+   *          Code to be interpreted for generating the animation
+   * @param dM
+   *          Debug level for showing debug messages
+   */
+  InterpreterThread(StickMotion sm, String text, int dM) {
     super();
-  }
-
-  public void interpreter(StickMotion sm, String text, int dM) {
     _code = text;
     _debugMode = dM;
     _sm = sm;
-    run();
   }
 
+  /**
+   * Method to run when the thread is started
+   */
   @Override
   public void run() {
     _results = sticky.Processor.run(_code, _debugMode);
     _sm.setResults(_results);
-    _sm.enablePlay();
+    StickMotion.scene.start();
+    _sm.disablePlay();
   }
 }
